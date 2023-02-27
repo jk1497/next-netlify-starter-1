b85e00cc641385f4e2cb874dbe571d4768a7d19e
Jonny Kidd <jk1497@yahoo.co.uk>
2023-02-27 20:21:51 +0900

    updated index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Drop from '@components/Dropdown'
-// import App from'@components/Wordlist'
-import CsvReader from'@components/CsvReader'
+import DropdownMenu from '../components/DropdownMenu';
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <main>
-      <Drop>
-      </Drop>
-      <CsvReader/>
-        <Header title="Believe in M" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-      {/* <Footer /> */}
-    </div>
+    <main>
+    <DropdownMenu/>
+    </main>
   )
 }
