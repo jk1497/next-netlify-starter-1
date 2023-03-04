d1353d00ab65ae67213141f2d74ee44a489fd7fa
Jonny Kidd <jk1497@yahoo.co.uk>
2023-03-04 14:06:57 +0900

    Added genres to DropdownMenu

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -14,8 +14,19 @@ function DropdownMenu() {
       <label htmlFor="dropdown">Preferred Genre:</label>
       <select id="dropdown" value={selectedOption} onChange={handleOptionChange}>
         <option value="Action">Action</option>
-        <option value="Horror">Horror</option>
+        <option value="Adventure">Adventure</option>
+        <option value="Animation">Animation</option>
         <option value="Comedy">Comedy</option>
+        <option value="Crime">Crime</option>
+        <option value="Drama">Drama</option>
+        <option value="Family">Family</option>
+        <option value="Fantasy">Fantasy</option>
+        <option value="Horror">Horror</option>
+        <option value="Mystery">Mystery</option>
+        <option value="Romance">Romance</option>
+        <option value="Science Fiction">Science Fiction</option>
+        <option value="Thriller">Thriller</option>
+        <option value="War">War</option>
       </select>
       <AnotherComponent genre={selectedOption} />
     </div>
