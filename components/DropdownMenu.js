7371017480a5e8c872c3851d3c1420f64245c866
Jonny Kidd <jk1497@yahoo.co.uk>
2023-03-04 14:30:42 +0900

    increased displayed words from 10->15

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -37,7 +37,7 @@ function AnotherComponent(props) {
 
   const selectedGenre = props.genre
 
-  const myList = words[selectedGenre].slice(0,10);
+  const myList = words[selectedGenre].slice(0,15);
 
   console.log(props)
 
